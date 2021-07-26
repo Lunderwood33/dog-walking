bf13ea5e084546b115c8a35891411b81c16f22d1
Lunderwood33 <donnie.underwood.11@gmail.com>
2021-07-26 13:36:45 -0500

    working on forloop to make walkers show their cities

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,4 +1,5 @@
 import { getWalkers } from "./database.js"
+import { getCities } from "./database.js"
 
 document.addEventListener(
     "click",
@@ -7,15 +8,22 @@ document.addEventListener(
         if (itemClicked.id.startsWith("walker")) {
             const [,walkerId] = itemClicked.id.split("--")
 
-            for (const walker of walkers) {
-                if (walker.id === parseInt(walkerId)) {
-                    window.alert(`${walker.name} services ${walker.city}`)
+            for (const city of cities) {
+                if (city.id === walkers.cityId) {
+                    
+                    for (const walker of walkers) {
+                        if (walker.id === parseInt(walkerId)) {
+                            window.alert(`${walker.name} services ${city.name}`)
+                        }
                 }
+            }
+            
             }
         }
     }
 )
 const walkers = getWalkers()
+const cities = getCities()
 
 
 export const Walkers = () => {
